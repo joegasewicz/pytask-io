ade5d51db5eda8617d4563112e132bad37b4e51a
Joe Gasewicz <jgasewicz@bink.com>
2020-01-22 09:43:39 +0000

    workers / queue

--- pytask_io/worker.py
+++ pytask_io/worker.py
@@ -5,7 +5,7 @@ async def worker_one(queue: asyncio.Queue):
     result = await queue.get()
     await asyncio.sleep(3)
     print("Worker: 1")
-    print(result)
+    print(result())
     queue.task_done()
 
 
@@ -13,7 +13,7 @@ async def worker_two(queue: asyncio.Queue):
     result = await queue.get()
     await asyncio.sleep(1)
     print("Worker: 2")
-    print(result)
+    print(result())
     queue.task_done()
 
 
@@ -21,7 +21,7 @@ async def worker_three(queue: asyncio.Queue):
     result = await queue.get()
     await asyncio.sleep(1)
     print("Worker: 3")
-    print(result)
+    print(result())
     queue.task_done()
 
 
@@ -30,15 +30,29 @@ async def main():
     # ----- Queue ------
     queue = asyncio.Queue()
 
-    queue.put_nowait("1 ------>>>> ")
-    queue.put_nowait("2 ------>>>> ")
-    queue.put_nowait("3 ------>>>> ")
+    def test_fnc_one():
+        return f"1 ------>>>> "
+
+    def test_fnc_two():
+        return f"2 ------>>>> "
+
+    def test_fnc_three():
+        return f"3 ------>>>> "
+
+    def test_fnc_four():
+        return f"4 ------>>>> "
+
+    queue.put_nowait(test_fnc_one)
+    queue.put_nowait(test_fnc_two)
+    queue.put_nowait(test_fnc_three)
+    queue.put_nowait(test_fnc_four)
 
     # --- worker tasks to process the queue concurrently
     # TODO Producer here ------>
     task_one = asyncio.create_task(worker_one(queue))
     task_two = asyncio.create_task(worker_two(queue))
     task_three = asyncio.create_task(worker_three(queue))
+    task_four = asyncio.create_task(worker_two(queue))
 
     # wait until queue is fully processed
     # TODO Consumer here ---->
@@ -48,6 +62,7 @@ async def main():
     task_one.cancel()
     task_two.cancel()
     task_three.cancel()
+    task_four.cancel()
 
     # wait until all worker tasks are cancelled
     await asyncio.gather(task_one, task_two, task_three)
