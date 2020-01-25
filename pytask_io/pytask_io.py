50820feb9eec91dc60a7b017a0bf396060d827ce
Joe Gasewicz <joegasewicz@gmail.com>
2020-01-25 15:57:43 +0000

    add_unit_of_work

--- pytask_io/pytask_io.py
+++ pytask_io/pytask_io.py
@@ -3,7 +3,7 @@ from typing import List, Callable
 import redis
 
 from pytask_io.client import client
-from pytask_io.task_queue import create_task_queue
+from pytask_io.task_queue import create_task_queue, serialize_unit_of_work
 
 
 class PyTaskIO:
@@ -15,9 +15,14 @@ class PyTaskIO:
         self.init_app()
 
     def add_unit_of_work(self, unit_of_work, *args):
-        """Add UOW to task queue"""
-
-
+        """
+        Adds units of work to the queue client
+        :param unit_of_work: A callable / executable Python function
+        :param args: The list of arguments required by `unit_of_work`
+        :return:
+        """
+        dumped_uow = serialize_unit_of_work(unit_of_work, *args)
+        self.queue_client.lpush("tasks", dumped_uow)
 
     def init_app(self):
         self.run_event_loop()
