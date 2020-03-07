fe16f0315e7104e7624df6238975f90f32ca03ca
Joe Gasewicz <joegasewicz@gmail.com>
2020-03-07 21:21:41 +0000

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pytask-io",
-    version="0.0.8",
+    version="0.0.9",
     description="An asynchronous Tasks Library using asyncio",
     packages=['pytask_io'],
     install_requires=[
