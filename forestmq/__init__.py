05d01e65f4a88fec954b73f22de077af4329d4cf
joegasewicz <joegasewicz@users.noreply.github.com>
2026-06-02 19:40:04 +0100

    #108

--- forestmq/__init__.py
+++ forestmq/__init__.py
@@ -1,6 +1,13 @@
-from tcp import TCP
+import asyncio
+
+from forestmq.tcp import TCP
+from forestmq.logger import print_preamble
 
 
 if __name__ == "__main__":
-   server = TCP()
-   server.serve()
+   print_preamble()
+   server = TCP(
+      host="127.0.0.1",
+      port=7171,
+   )
+   asyncio.run(server.serve())
--- forestmq/tcp.py
+++ forestmq/tcp.py
@@ -1,8 +1,21 @@
+import asyncio
+
+from forestmq.logger import log
+
+
 class TCP:
 
-    def __init__(self):
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+
+    async def handler(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         pass
 
 
-    def serve(self):
-        pass
+    async def serve(self):
+        s = await asyncio.start_server(self.handler, self.host, self.port)
+        log.info(f"Starting server on http://{self.host}:{self.port}")
+
+        async with s:
+            await s.serve_forever()
